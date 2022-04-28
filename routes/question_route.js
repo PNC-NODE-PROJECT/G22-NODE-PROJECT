5d3fd17f5bae296aac631261c77bcf5c6c4a8a5b
Oun.BAV <Oun.BAV@PNC.LAN>
2022-04-28 18:40:59 +0700

    #2

---
+++ routes/question_route.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router=express.Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+let PATH = "./data/questions.json";
+
+let read_file = (filename) =>JSON.parse(fs.readFileSync(filename));
+let write_file = (filename,data) =>fs.writeFileSync(filename,data);
+
+let questions = read_file(PATH);
+
+let model_question = require("../model/model.js");
+
+// get all questions ====================================================================
+router.get("/",(req,res)=>{
+  res.send(model_question.GetAllQuestions())
+})
+// get one questions ====================================================================
+router.get("/:id",(req,res)=>{
+    let id = req.params.id
+    res.send(model_question.Get_One_Questions(id));
+})
+// add questions to quize================================================================
+
+router.post('/',(req,res)=>{
+    let new_question = {
+        id:uuidv4(),
+        title:req.body.title,
+        choiceA:req.body.choiceA,
+        choiceB:req.body.choiceB,
+        choiceC:req.body.choiceC,
+        choiceD:req.body.choiceD,
+        correct:req.body.correct,
+    }
+    res.send(model_question.create_Question(new_question));
+})
+
+// delete questions=====================================================================
+
+router.delete('/:id',(req,res)=>{
+    let id = req.params.id;
+    res.send(model_question.delete_One_Question(id));
+})
+
+// update questions================================================================
+
+router.put('/:id',(req,res)=>{
+    let id = req.params.id;
+    let mybody = req.body;
+    res.send(model_question.Updat_Questions(id,mybody));
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,73 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 const { v4: uuidv4 } = require('uuid');
 app.listen(PORT,()=>{console.log("http://localhost:"+PORT)});
-let PATH = "./data/questions.json";
 
-let read_file = (filename) =>JSON.parse(fs.readFileSync(filename));
-let write_file = (filename,data) =>JSON.parse(fs.writeFileSync(filename,data));
 
-let questions = read_file(PATH);
-
-// get all questions ====================================================================
-app.get("/questions",(req,res)=>{
-    if (questions.length > 0){
-        res.send(questions);
-    }
-    else
-    {
-        res.send("No questions yet !!")
-    }
-})
-// get one questions ====================================================================
-app.get("/questions/:id",(req,res)=>{
-    let id = req.params.id
-    let index = questions.findIndex(questions => questions.id === id);
-    if (index !== undefined){
-        res.send(questions[index])
-    }
-    else{
-        res.send("Id not found !!")
-    }
-})
-// add questions to quize================================================================
-
-app.post('/questions',(req,res)=>{
-    let new_question = {
-        id:uuidv4(),
-        title:req.body.title,
-        choiceA:req.body.choiceA,
-        choiceB:req.body.choiceB,
-        choiceC:req.body.choiceC,
-        choiceD:req.body.choiceD,
-        correct:req.body.correct,
-    }
-    questions.push(new_question);
-    res.send(questions);
-})
-
-// delete questions=====================================================================
-
-app.delete('/questions/:id',(req,res)=>{
-    let id = req.params.id;
-    let index = questions.findIndex(questions => questions.id === id);
-    if(index != -1){
-        questions.splice(index, 1);
-        res.send({"message": "you have deleted"})
-    }
-    else{
-        res.status(404).send({"message": "Id not found"});
-    }
-})
-
-// update questions================================================================
-
-app.path('/questions/:id',(req,res)=>{
-    let id = req.params.id;
-    let index = questions.findIndex(questions => questions.id === id);
-    questions[index].choiceA =req.body.title;
-    questions[index].choiceA =req.body.choiceA;
-    questions[index].choiceB =req.body.choiceB;
-    questions[index].choiceC =req.body.choiceC;
-    questions[index].choiceD =req.body.choiceD;
-    questions[index].correct =req.body.correct;
-})
+let questions = require("./routes/question_route")
+app.use("/questions",questions);
