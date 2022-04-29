62a8236ee4073c7bc8d826d5ef7b3706f034bc9a
lidathy-real <lidathy9@gmail.com>
2022-04-29 16:16:09 +0700

    #3 remove question #6 edit

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 app.use(express.json());
+app.use(cors({
+    origin:"*"
+}))
 const { v4: uuidv4 } = require('uuid');
 app.listen(PORT,()=>{console.log("http://localhost:"+PORT)});
 app.use(express.static("./public"));
