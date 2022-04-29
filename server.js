d980bc4f357cd194c855e15fe886baed382f30d5
Oun.BAV <Oun.BAV@pnc.lan>
2022-04-29 09:28:32 +0700

    #22 display quiz

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 app.use(express.json());
 const { v4: uuidv4 } = require('uuid');
+app.use(cors({
+    origin:"*"
+}))
 app.listen(PORT,()=>{console.log("http://localhost:"+PORT)});
 app.use(express.static("./public"));
 let questions = require("./routes/question_route")
